3bf45870196d2fbc8918c351a6392f834bfec2a9
DerN3rd <dern3rd@outlook.at>
2025-07-15 16:34:43 +0200

    Überarbeitung des Media Uploader-Systems zur Vereinfachung der Integration und Verbesserung der Benutzeroberfläche

--- library/visual-editor/media-uploader.php
+++ library/visual-editor/media-uploader.php
@@ -1,96 +1,147 @@
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
-
-<head>
-
-<meta charset="<?php echo get_bloginfo('charset'); ?>" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-
-
 <?php
+// Direct Media Uploader for Padma Visual Editor
+// Loads WordPress media uploader directly without extra steps
 
-$GLOBALS['wp_media_view_strings'] = array();
-$GLOBALS['wp_media_view_settings'] = array();
-
-/* Begin kludge to fix issue with WP string localize in some cases.  See: https://core.trac.wordpress.org/ticket/24724 */
-function padma_media_view_settings_grabber( $settings ) {
-
-	$GLOBALS['wp_media_view_settings'] = $settings;
-
-	return $settings;
-
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
 }
-add_filter( 'media_view_settings', 'padma_media_view_settings_grabber' );
 
-
-function padma_media_view_strings_grabber($strings) {
-
-	$strings['attachmentsList'] = '';
-	$GLOBALS['wp_media_view_strings'] = $strings;
-
-	return $strings;
-
+// Load WordPress
+if (!function_exists('wp_enqueue_script')) {
+    require_once('../../../../../../wp-load.php');
 }
-add_filter('media_view_strings', 'padma_media_view_strings_grabber');
 
-wp_enqueue_media();
+// Redirect directly to media uploader
+$media_url = admin_url('media-upload.php?type=image&TB_iframe=true&width=100%&height=100%');
 
-$GLOBALS['wp_media_view_strings']['settings'] = $GLOBALS['wp_media_view_settings'];
-
-//wp_enqueue_script( 'media-editor', padma_url() . '/library/visual-editor/scripts-src/deps/media-editor.js', array( 'jquery' ) );
-wp_localize_script( 'media-editor', '_wpMediaViewsL10n', $GLOBALS['wp_media_view_strings'] );
-
-//wp_register_script( 'media-views', padma_url() . '/library/visual-editor/scripts-src/deps/media-views.js' );
-wp_localize_script( 'media-views', '_wpMediaViewsL10n', $GLOBALS['wp_media_view_strings'] );
-/* End Kludge */
-
-wp_enqueue_style('open-sans');
-
-wp_print_styles();
+// Custom send_to_editor function to handle the selection
 ?>
-
-<style type="text/css">
-	.media-modal {
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-	}
-
-	.media-modal-close {
-		display: none;
-	}
-
-	.media-frame-router {
-		top: 5px;
-	}
-
-	.media-frame-content {
-		top: 39px;
-	}
-	.media-frame-actions-heading{
-		display: none;
-	}
-	h2.media-attachments-filter-heading{}
-	label[for='media-attachment-date-filters']{
-		float: left;
-	    padding: 0;
-	    margin-top: 25px;
-	}
-	#media-attachment-date-filters{
-		float: left;
-	    margin-top: 15px;
-	    margin-left: 10px;
-	}
-</style>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Media Uploader</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        #media-frame {
+            width: 100%;
+            height: 100vh;
+            border: none;
+        }
+        /* Hide MarketPress cart and other unwanted elements */
+        .mp_cart_widget_content,
+        .mp_cart_widget,
+        .marketpress-cart,
+        .mp-cart,
+        .floating-cart,
+        .cart-widget,
+        .woocommerce-cart,
+        .cart-contents,
+        .cart-icon,
+        .header-cart,
+        .mini-cart,
+        .widget_shopping_cart,
+        .shopping-cart-widget {
+            display: none !important;
+            visibility: hidden !important;
+        }
+    </style>
 </head>
 <body>
-<?php
-wp_print_media_templates();
-
-
-wp_print_scripts();
-?>
+    <iframe id="media-frame" src="<?php echo $media_url; ?>"></iframe>
+    
+    <script>
+        // Override send_to_editor function in the iframe
+        window.addEventListener('message', function(event) {
+            if (event.data && event.data.type === 'media_selected') {
+                var imgUrl = event.data.url;
+                var filename = event.data.filename;
+                
+                // Call parent callback
+                if (window.parent && window.parent.imageUploaderCallback) {
+                    window.parent.imageUploaderCallback(imgUrl, filename);
+                }
+                
+                // Close the box
+                if (window.parent && window.parent.closeBox) {
+                    window.parent.closeBox('input-image', true);
+                }
+            }
+        });
+        
+        // Wait for iframe to load and inject our script
+        document.getElementById('media-frame').addEventListener('load', function() {
+            try {
+                var iframe = document.getElementById('media-frame');
+                var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+                var iframeWindow = iframe.contentWindow;
+                
+                // Override send_to_editor in the iframe
+                iframeWindow.send_to_editor = function(html) {
+                    // Extract image URL from HTML
+                    var tempDiv = iframeDoc.createElement('div');
+                    tempDiv.innerHTML = html;
+                    
+                    var imgUrl = '';
+                    var img = tempDiv.querySelector('img');
+                    if (img) {
+                        imgUrl = img.getAttribute('src');
+                    } else {
+                        // Try to extract from link
+                        var link = tempDiv.querySelector('a');
+                        if (link) {
+                            imgUrl = link.getAttribute('href');
+                        }
+                    }
+                    
+                    if (imgUrl) {
+                        var filename = imgUrl.split('/').pop();
+                        
+                        // Call parent callback
+                        if (window.parent && window.parent.imageUploaderCallback) {
+                            window.parent.imageUploaderCallback(imgUrl, filename);
+                        }
+                        
+                        // Close the box
+                        if (window.parent && window.parent.closeBox) {
+                            window.parent.closeBox('input-image', true);
+                        }
+                    }
+                };
+                
+                // Hide unwanted elements in iframe
+                var hideElements = [
+                    '.mp_cart_widget_content',
+                    '.mp_cart_widget',
+                    '.marketpress-cart',
+                    '.mp-cart',
+                    '.floating-cart',
+                    '.cart-widget',
+                    '.woocommerce-cart',
+                    '.cart-contents',
+                    '.cart-icon',
+                    '.header-cart',
+                    '.mini-cart',
+                    '.widget_shopping_cart',
+                    '.shopping-cart-widget'
+                ];
+                
+                hideElements.forEach(function(selector) {
+                    var elements = iframeDoc.querySelectorAll(selector);
+                    elements.forEach(function(el) {
+                        el.style.display = 'none';
+                        el.style.visibility = 'hidden';
+                    });
+                });
+                
+            } catch (e) {
+                console.log('Cross-origin iframe access blocked, using fallback');
+            }
+        });
+    </script>
 </body>
 </html>
--- library/visual-editor/scripts-src/util.image-uploader.js
+++ library/visual-editor/scripts-src/util.image-uploader.js
@@ -2,6 +2,9 @@ define(['jquery', 'underscore'], function($, _) {
 
 	openImageUploader = function(callback) {
 
+		// Store callback globally for the iframe to access
+		window.imageUploaderCallback = callback;
+
 		if ( !boxExists('input-image') ) {
 
 			if ( isNaN(Padma.viewModels.layoutSelector.currentLayout()) )
@@ -13,7 +16,8 @@ define(['jquery', 'underscore'], function($, _) {
 				description: 'Upload or select an image',
 				src: Padma.homeURL + '/?padma-trigger=media-uploader',
 				load: function() {
-					initiateImageUploader(callback);
+					// No need to initiate complex uploader, just wait for iframe to load
+					console.log('Media uploader iframe loaded');
 				},
 				width: $(window).width() - 200,
 				height: $(window).height() - 200,
@@ -40,60 +44,7 @@ define(['jquery', 'underscore'], function($, _) {
 
 	}
 
-	initiateImageUploader = function(callback) {
-
-		/* Check if iframe body has iframe-loaded class which is added via inline script in the footer of the iframe */
-		if (
-			!$('#box-input-image iframe').length
-			|| typeof $('#box-input-image iframe')[0].contentWindow.wp == 'undefined'
-			|| typeof $('#box-input-image iframe')[0].contentWindow.wp.media == 'undefined'
-			|| typeof $('#box-input-image iframe')[0].contentWindow.wp.media() == 'undefined'
-		) {
-
-			return setTimeout(function() {
-				initiateImageUploader(callback);
-			}, 100);
-
-		}
-
-		wpMedia = $('#box-input-image iframe')[0].contentWindow.wp.media;
-		var currentLayoutFragments = Padma.viewModels.layoutSelector.currentLayout().split('-');
-
-		/* If the current layout is a WordPress "post" then associate all attachments uploaded with the post */
-		if (_.first(currentLayoutFragments) == 'single' && !isNaN(_.last(currentLayoutFragments)) ) {
-			wpMedia.model.settings.post.id = _.last(currentLayoutFragments);
-		}
-
-		wpMedia.frames = {
-			file_frame: wpMedia({
-				title: '',
-				button: {
-					text: 'Use Image'
-				},
-				multiple: false
-			})
-		};
-
-		wpMedia.frames.file_frame.on( 'select', function() {
-			// We set multiple to false so only get one image from the uploader
-			attachment = wpMedia.frames.file_frame.state().get('selection').first().toJSON();
-
-			if ( typeof url == 'undefined' )
-				var url = attachment.url;
-
-			var filename = url.split('/')[url.split('/').length-1];
-
-			callback(url, filename);
-
-			parent.closeBox('input-image', true);
-		});
-
-		wpMedia.frames.file_frame.on('escape', function() {
-			parent.closeBox('input-image', true);
-		});
-
-		return wpMedia.frames.file_frame.open();
-
-	}
+	// Callback function is now handled by the iframe directly
+	// No need for complex WordPress Media API integration
 
 });
